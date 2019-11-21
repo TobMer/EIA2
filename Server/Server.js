3fc703377e8fae800e8140627b6cd6ec2f82f610
TobMer <48830884+TobMer@users.noreply.github.com>
2019-11-21 12:29:15 +0100

    x number

--- Server/Server.js
+++ Server/Server.js
@@ -1,2 +1,7 @@
 "use strict";
+console.log("Du bist wunderbar und EIA macht Spaß");
+let x = 0;
+console.log(x);
+x++;
+console.warn(x);
 //# sourceMappingURL=Server.js.map
---
+++ Server/Server.ts
@@ -0,0 +1,6 @@
+console.log("Du bist wunderbar und EIA macht Spaß");
+
+let x: number = 0;
+console.log(x);
+x++;
+console.warn(x);
