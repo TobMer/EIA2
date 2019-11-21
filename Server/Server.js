fa54fc799d65284f853895d0a933d09fa584f20b
TobMer <48830884+TobMer@users.noreply.github.com>
2019-11-21 15:11:05 +0100

    client

--- Server/Server.js
+++ Server/Server.js
@@ -4,4 +4,17 @@ let x = 0;
 console.log(x);
 x++;
 console.warn(x);
+setTimeout(handleTimeout, 2000);
+console.log(process.env.COMPUTERNAME);
+console.log(process.env.USERNAME);
+console.log(process.env.PORT);
+console.log(process.argv);
+console.log("Hallo" + process.argv[2]);
+process.addListener("exit", handleExit);
+function handleTimeout(_event) {
+    console.log("Timeout");
+}
+function handleExit(_event) {
+    console.log("Tschüss");
+}
 //# sourceMappingURL=Server.js.map
--- Server/Server.ts
+++ Server/Server.ts
@@ -4,3 +4,22 @@ let x: number = 0;
 console.log(x);
 x++;
 console.warn(x);
+setTimeout(handleTimeout, 2000);
+console.log(process.env.COMPUTERNAME);
+console.log(process.env.USERNAME);
+console.log(process.env.PORT);
+
+console.log(process.argv);
+console.log("Hallo" + process.argv[2]);
+
+process.addListener("exit", handleExit);
+
+function handleTimeout(
+    _event: Event): void {
+    console.log("Timeout");
+}
+
+function handleExit(_event: Event): void {
+
+    console.log("Tschüss");
+}
