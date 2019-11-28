805c12255555b45f46a1c266b46b9256cd149b00
TobMer <48830884+TobMer@users.noreply.github.com>
2019-11-28 14:13:16 +0100

    Add files via upload

---
+++ Aufgabe06/client/CocktailBar.js
@@ -0,0 +1,74 @@
+"use strict";
+var L06_CocktailBar;
+(function (L06_CocktailBar) {
+    window.addEventListener("load", handleLoad);
+    // const url: string = "index.html";
+    let form;
+    //const url: string = "https://eia2-inverted-dev.herokuapp.com";
+    let url = "http://localhost:5001";
+    async function handleLoad(_event) {
+        console.log("Init");
+        let response = await fetch("Data.json");
+        let offer = await response.text();
+        let data = JSON.parse(offer);
+        L06_CocktailBar.generateContent(data);
+        form = document.querySelector("form");
+        let slider = document.querySelector("input#amount");
+        let submit = document.querySelector("button[type=submit]");
+        console.log(submit);
+        submit.addEventListener("click", sendOrder);
+        form.addEventListener("change", handleChange);
+        slider.addEventListener("input", displayAmount);
+        displayOrder();
+    }
+    async function sendOrder(_event) {
+        // return; // for testing
+        console.log("SendOrder");
+        _event.preventDefault();
+        let formData = new FormData(document.querySelector("form"));
+        //@ts-ignore
+        let query = new URLSearchParams(formData);
+        let response = await fetch(url + "?" + query.toString());
+        let responseText = await response.text();
+        alert(responseText);
+    }
+    function handleChange(_event) {
+        displayOrder();
+    }
+    function displayOrder() {
+        // let inputs: NodeListOf<HTMLInputElement> = document.querySelectorAll("input");
+        // console.log(inputs);
+        let price = 0;
+        let order = document.querySelector("div#order");
+        order.innerHTML = "";
+        let formData = new FormData(document.querySelector("form"));
+        // console.group("Order");
+        for (let entry of formData) {
+            // console.log(entry);
+            let selector = "[value='" + entry[1] + "']"; // "[name='" + entry[0] + "'][value='" + entry[1] + "']";
+            let item = document.querySelector(selector);
+            let itemPrice = Number(item.getAttribute("price"));
+            switch (entry[0]) {
+                case "Amount":
+                    break;
+                case "Drink":
+                    let amount = Number(formData.get("Amount"));
+                    itemPrice = amount * itemPrice;
+                    order.innerHTML += amount + " L " + item.value + ": €" + itemPrice + "<br>";
+                    break;
+                default:
+                    order.innerHTML += item.value + ": €" + itemPrice.toFixed(2) + "<br>";
+            }
+            // console.log(item);
+            price += itemPrice;
+        }
+        // console.groupEnd();
+        order.innerHTML += "<p><strong>Total: €" + price.toFixed(2);
+    }
+    function displayAmount(_event) {
+        let progress = document.querySelector("progress");
+        let amount = _event.target.value;
+        progress.value = parseFloat(amount);
+    }
+})(L06_CocktailBar || (L06_CocktailBar = {}));
+//# sourceMappingURL=CocktailBar.js.map
